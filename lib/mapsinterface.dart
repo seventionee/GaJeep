6602a976f50cfe1cf8aeee5478b3e0a6284d37b1
seventionee <seventionee@gmail.com>
2023-04-02 00:55:02 +0800

    added search bar for places, set min and max zoom, disabled map compass

--- lib/mapsinterface.dart
+++ lib/mapsinterface.dart
@@ -4,7 +4,9 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:search_map_place_updated/search_map_place_updated.dart';
 
+// ignore: use_key_in_widget_constructors
 class Mapsinterface extends StatefulWidget {
   static const routeName = '/mapsinterface';
 
@@ -69,26 +71,55 @@ class _Mapsinterface extends State<Mapsinterface> {
         }
         return MaterialApp(
           home: Scaffold(
-            body: GoogleMap(
-              onMapCreated: (GoogleMapController controller) async {
-                _controller = controller;
-                _controller.setMapStyle(snapshot.data!);
-              },
-              mapType: MapType.normal,
-              myLocationEnabled: true,
-              initialCameraPosition: const CameraPosition(
-                target: LatLng(10.3156173, 123.882969),
-                zoom: 19,
-              ),
-              zoomControlsEnabled: false, // Remove zoom controls
-              myLocationButtonEnabled: false, // Remove location button
-              mapToolbarEnabled: false,
-            ),
-            floatingActionButton: FloatingActionButton(
-              backgroundColor: Theme.of(context).colorScheme.secondary,
-              onPressed: _showUserLocation,
-              tooltip: 'Show user location',
-              child: const Icon(Icons.location_searching),
+            body: Stack(
+              children: [
+                GoogleMap(
+                  onMapCreated: (GoogleMapController controller) async {
+                    _controller = controller;
+                    _controller.setMapStyle(snapshot.data!);
+                  },
+                  compassEnabled: false,
+                  minMaxZoomPreference: const MinMaxZoomPreference(17, 19),
+                  mapType: MapType.normal,
+                  myLocationEnabled: true,
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(10.3156173, 123.882969),
+                    zoom: 19,
+                  ),
+                  zoomControlsEnabled: false, // Remove zoom controls
+                  myLocationButtonEnabled: false, // Remove location button
+                  mapToolbarEnabled: false,
+                ),
+                Positioned(
+                  top: 50,
+                  left: 16,
+                  right: 16,
+                  child: SearchMapPlaceWidget(
+                    apiKey: 'AIzaSyBOS4cS8wIYV2tRBhtf5O2hnIZ1Iley9Jc',
+                    language: 'en',
+                    bgColor: Colors.white,
+                    iconColor: Theme.of(context).colorScheme.primary,
+                    textColor: Colors.black,
+                    placeholder: 'Where do you want to go to?',
+                    onSelected: (Place place) async {
+                      final geolocation = await place.geolocation;
+                      final cameraUpdate =
+                          CameraUpdate.newLatLng(geolocation!.coordinates!);
+                      _controller.animateCamera(cameraUpdate);
+                    },
+                  ),
+                ),
+                Positioned(
+                  bottom: 16,
+                  right: 16,
+                  child: FloatingActionButton(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    onPressed: _showUserLocation,
+                    tooltip: 'Show user location',
+                    child: const Icon(Icons.location_searching),
+                  ),
+                ),
+              ],
             ),
           ),
         );
