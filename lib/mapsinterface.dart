c2365f08b40540ac8a0e0318e9567a14921fba64
seventionee <seventionee@gmail.com>
2023-04-11 22:58:40 +0800

    additional chubacca

--- lib/mapsinterface.dart
+++ lib/mapsinterface.dart
@@ -204,27 +204,8 @@ class _Mapsinterface extends State<Mapsinterface> {
                       mapToolbarEnabled: false,
                     ),
                     Positioned(
-                      top: 50,
-                      left: 16,
-                      right: 16,
-                      child: SearchMapPlaceWidget(
-                        apiKey: 'AIzaSyBOS4cS8wIYV2tRBhtf5O2hnIZ1Iley9Jc',
-                        language: 'en',
-                        bgColor: Colors.white,
-                        iconColor: Theme.of(context).colorScheme.primary,
-                        textColor: Colors.black,
-                        placeholder: 'Where do you want to go to?',
-                        onSelected: (Place place) async {
-                          final geolocation = await place.geolocation;
-                          final cameraUpdate =
-                              CameraUpdate.newLatLng(geolocation!.coordinates!);
-                          _controller.animateCamera(cameraUpdate);
-                        },
-                      ),
-                    ),
-                    Positioned(
-                        top: 30,
-                        left: 16,
+                        bottom: 90,
+                        right: 16,
                         child: Container(
                           //style for menu button
                           decoration: BoxDecoration(
@@ -233,25 +214,16 @@ class _Mapsinterface extends State<Mapsinterface> {
                               width: 1.0, // set the border width
                             ),
                             borderRadius: BorderRadius.circular(
-                                25.0), // set the border radius
+                                50.0), // set the border radius
                           ),
-                          child: FloatingActionButton.extended(
+                          child: FloatingActionButton(
                             heroTag: null,
-                            label: const Text(
-                              'Menu',
-                              style: TextStyle(
-                                fontFamily: 'Epilogue', //font style
-                                fontWeight: FontWeight.w400,
-                                fontSize: 20.0,
-                                color: Colors.black,
-                              ),
-                            ),
                             foregroundColor: const Color.fromARGB(255, 0, 0, 0),
                             backgroundColor: primaryColor,
                             onPressed: () {
                               Scaffold.of(context).openDrawer();
                             },
-                            icon: const Icon(Icons.menu),
+                            child: const Icon(Icons.menu_rounded),
                           ),
                         )),
                     Positioned(
@@ -274,6 +246,25 @@ class _Mapsinterface extends State<Mapsinterface> {
                             child: const Icon(Icons.location_searching),
                           ),
                         )),
+                    Positioned(
+                      top: 50,
+                      left: 16,
+                      right: 16,
+                      child: SearchMapPlaceWidget(
+                        apiKey: 'AIzaSyBOS4cS8wIYV2tRBhtf5O2hnIZ1Iley9Jc',
+                        language: 'en',
+                        bgColor: Colors.white,
+                        iconColor: primaryColor,
+                        textColor: Colors.black,
+                        placeholder: 'Where do you want to go to?',
+                        onSelected: (Place place) async {
+                          final geolocation = await place.geolocation;
+                          final cameraUpdate =
+                              CameraUpdate.newLatLng(geolocation!.coordinates!);
+                          _controller.animateCamera(cameraUpdate);
+                        },
+                      ),
+                    ),
                   ],
                 );
               }),
