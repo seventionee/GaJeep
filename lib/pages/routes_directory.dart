45ed69c93b7fc2ad005222f7aa6abecbf296df80
seventionee <seventionee@gmail.com>
2023-05-02 00:31:07 +0800

    intial design for routes directory

--- lib/pages/routes_directory.dart
+++ lib/pages/routes_directory.dart
@@ -1,10 +1,12 @@
+import '../providers/polylinesdrawer.dart';
+import '../component/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../providers/polylinesdrawer.dart';
 
 class RoutesDirectory extends StatelessWidget {
   const RoutesDirectory({Key? key}) : super(key: key);
   static const routeName = '/routesdirectory';
+
   Future<List<QueryDocumentSnapshot>> fetchRoutes() async {
     CollectionReference collection =
         FirebaseFirestore.instance.collection('Routes');
@@ -16,7 +18,16 @@ class RoutesDirectory extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Routes Directory'),
+        leading: const Icon(Icons.arrow_back, color: Colors.black),
+        title: const Text(
+          'Routes Directory',
+          style: TextStyle(
+            fontFamily: 'Epilogue', //font style
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+            color: Colors.black,
+          ),
+        ),
       ),
       body: FutureBuilder<List<QueryDocumentSnapshot>>(
         future: fetchRoutes(),
@@ -30,45 +41,105 @@ class RoutesDirectory extends StatelessWidget {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
 
-          return GridView.builder(
-            padding: const EdgeInsets.all(10),
+          return ListView.builder(
+            padding: const EdgeInsets.all(8.0),
             itemCount: snapshot.data!.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-            ),
             itemBuilder: (BuildContext ctx, int index) {
               String routeNumber = snapshot.data![index]['Route Number'];
               String routeDescription =
                   snapshot.data![index]['Route Description'];
-
-              return Card(
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      width: 1.0,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(25.0)),
+                    color: primaryColor,
+                  ),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Route Number: $routeNumber'),
-                      Text('Route Description: $routeDescription'),
-                      const SizedBox(height: 10),
-                      StreamBuilder<int>(
-                        stream: getActiveVehiclesForRoute(routeNumber),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<int> activeVehiclesSnapshot) {
-                          if (activeVehiclesSnapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const CircularProgressIndicator();
-                          }
-                          if (activeVehiclesSnapshot.hasError) {
-                            return Text(
-                                'Error: ${activeVehiclesSnapshot.error}');
-                          }
-                          return Text(
-                              'Active Vehicles: ${activeVehiclesSnapshot.data}');
-                        },
+                      ListTile(
+                        title: Text(
+                          'Route Number: $routeNumber',
+                          style: const TextStyle(
+                            fontFamily: 'Epilogue', //font style
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          ),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Route Description: $routeDescription',
+                              style: const TextStyle(
+                                fontFamily: 'Epilogue', //font style
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            StreamBuilder<int>(
+                              stream: getActiveVehiclesForRoute(routeNumber),
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<int> activeVehiclesSnapshot) {
+                                if (activeVehiclesSnapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  return const CircularProgressIndicator();
+                                }
+                                if (activeVehiclesSnapshot.hasError) {
+                                  return Text(
+                                      'Error: ${activeVehiclesSnapshot.error}');
+                                }
+                                return Text(
+                                  'Active Vehicles: ${activeVehiclesSnapshot.data}',
+                                  style: const TextStyle(
+                                    fontFamily: 'Epilogue', //font style
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 20.0,
+                                    color: Colors.black,
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: TextButton(
+                          onPressed: () {
+                            // Handle 'View Route' button press
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                secondaryColor),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                side: const BorderSide(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            'View Route',
+                            style: TextStyle(
+                              fontFamily: 'Epilogue', //font style
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
                       ),
                     ],
                   ),
