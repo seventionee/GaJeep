9388210eb8d9694e65d50d407170ca1d01975dcc
seventionee <seventionee@gmail.com>
2023-05-01 15:05:32 +0800

    initial setup

--- lib/providers/polylinesdrawer.dart
+++ lib/providers/polylinesdrawer.dart
@@ -4,6 +4,20 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import '../providers/route_details.dart';
 
+// Modify the function to accept routeNumber
+Future<int> getActiveVehiclesForRoute(String routeNumber) async {
+  CollectionReference collection =
+      FirebaseFirestore.instance.collection('Routes');
+  DocumentSnapshot routeDoc = await collection.doc(routeNumber).get();
+
+  QuerySnapshot vehicleSnapshot = await routeDoc.reference
+      .collection('Vehicles')
+      .where('Status', isEqualTo: 'Active')
+      .get();
+
+  return vehicleSnapshot.docs.length;
+}
+
 Future<List<Polyline>> getPolylinesFromFirestore(BuildContext context) async {
   List<Polyline> polylines = [];
   int polylineIdCounter = 1;
@@ -24,7 +38,7 @@ Future<List<Polyline>> getPolylinesFromFirestore(BuildContext context) async {
         .toList();
     debugPrint('Polyline fetch for $routeNumber: $latLngPoints');
 
-    //polyline color auto adjustible from rgb
+    //polyline color auto adjustable from rgb
     ui.Color polylineColor = ui.Color.fromARGB(
       255,
       HSVColor.fromAHSV(1.0, hueStep * (polylineIdCounter - 1), 1.0, 1.0)
@@ -38,6 +52,10 @@ Future<List<Polyline>> getPolylinesFromFirestore(BuildContext context) async {
           .blue,
     );
 
+    // Call the modified function and print the active vehicles for the specific route
+    int activeVehiclesForRoute = await getActiveVehiclesForRoute(routeNumber);
+    debugPrint('Active vehicles for $routeNumber: $activeVehiclesForRoute');
+
     Polyline polyline = Polyline(
         polylineId: PolylineId(polylineIdCounter.toString()),
         points: latLngPoints,
@@ -47,8 +65,7 @@ Future<List<Polyline>> getPolylinesFromFirestore(BuildContext context) async {
         onTap: () {
           debugPrint('Polyline is TAPPED!');
           showDialog(
-            context:
-                context, // Add BuildContext variable to be passed in the function
+            context: context,
             builder: (BuildContext context) {
               return RouteDetailsModal(
                 routeName: routeNumber,
